88996d2f5a841ceed2753531cb8376dca2fcca01
miki208 <miloss208@gmail.com>
2016-12-04 19:09:39 +0100

    Add data dump for tables korisnik, predavac, ucesnik

---
+++ podaci/online_kursevi_predavac.sql
@@ -0,0 +1,30 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: online_kursevi
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `predavac`
+--
+
+INSERT INTO `predavac` VALUES ('Univerzitet u Kragujevcu','anamarija.kovacevic768'),('Univerzitet u Pristini','anamarija.mitic489'),('Univerzitet u Pristini','anamarija.zoric350'),('Univerzitet u Pristini','andrija.maric483'),('Univerzitet u Novom Sadu','andrija.stojadinovic559'),('Univerzitet u Nisu','dragan.todorovic666'),('Univerzitet u Pristini','ivana.stanojevic934'),('Univerzitet u Novom Sadu','ivana.stefanovic724'),('Univerzitet u Kragujevcu','marija.saric981'),('Univerzitet u Beogradu','marija.stanojevic227'),('Univerzitet u Kragujevcu','marija.stojanovic986'),('Univerzitet u Kragujevcu','marko.maric132'),('Univerzitet u Novom Sadu','milos.kovacevic728'),('Univerzitet u Kragujevcu','milos.popov423'),('Univerzitet u Novom Sadu','milos.popov454'),('Univerzitet u Beogradu','milos.zoric937'),('Univerzitet u Nisu','mirko.stankovic544'),('Univerzitet u Nisu','mirko.todorovic191'),('Univerzitet u Beogradu','miroslav.petrovic599'),('Univerzitet u Kragujevcu','miroslav.saric310'),('Univerzitet u Pristini','nemanja.kovacevic487'),('Univerzitet u Pristini','nemanja.popovic402'),('Univerzitet u Nisu','nemanja.saric549'),('Univerzitet u Pristini','nemanja.stojanovic300'),('Univerzitet u Kragujevcu','tanja.maric337'),('Univerzitet u Nisu','tanja.maric873'),('Univerzitet u Beogradu','tanja.markovic641'),('Univerzitet u Nisu','tanja.zoric785'),('Univerzitet u Pristini','teodora.todorovic323'),('Univerzitet u Novom Sadu','una.stojanovic708');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed
