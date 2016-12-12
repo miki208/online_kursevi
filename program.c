4fc4f7ce44dd5c51ce21b7e48d8b50033eff28e0
miki208 <miloss208@gmail.com>
2016-12-12 23:17:42 +0100

    Update program.c (add error handling function)

--- program.c
+++ program.c
@@ -5,6 +5,19 @@
 #include <stdarg.h>
 #include <errno.h>
 
+void error_fatal(char *format, ...);
+
 int main() {
     return 0;
+}
+
+void error_fatal (char* format, ...)
+{
+  va_list arguments;
+
+  va_start (arguments, format);
+  vfprintf (stderr, format, arguments);
+  va_end (arguments);
+
+  exit (EXIT_FAILURE);
 }
