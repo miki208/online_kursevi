b035e08a468fbb9e2fc71afaf1e6f02ebf2a41e9
miki208 <miloss208@gmail.com>
2016-12-13 00:15:57 +0100

    Update program.c (add menu)

--- program.c
+++ program.c
@@ -13,10 +13,45 @@ const char* password = "";
 const char* db = "online_kursevi";
 
 int main() {
+    int quit = 0;
     MYSQL *conn = mysql_init(NULL);
     
     if(mysql_real_connect(conn, host, user, password, db, 0, NULL, 0) == NULL)
         error_fatal("Greska u konekciji: %s\n", mysql_error(conn));
+    
+    while(!quit) {
+        int opcija;
+        
+        printf("Odaberite jednu od opcija:\n");
+        printf("\t[1] - Izlistati informacije o korisniku, kurseve koje slusa i njihove informacije\n");
+        printf("\t[2] - Izlistati informacije o ucesnicima koji su dobili sertifikat, i informacije o odgovarajucim kursevima\n");
+        printf("\t[3] - Registrovati novog ucesnika\n");
+        printf("\t[4] - Dodati novi ciklus\n");
+        printf("\t[5] - Prijavljivanje ucesnika za kurs\n");
+        printf("\t[6] - Azuriranje progresa\n");
+        printf("\t[7] - Izlaz\n");
+        
+        scanf("%d", &opcija);
+        switch(opcija) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                quit = 1;
+                break;
+            default:
+                printf("Ova opcija ne postoji\n");
+        }
+    }
     
     mysql_close(conn);
     
