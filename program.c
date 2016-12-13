9e73a319b10db0c8844f79f7e142f58042f94612
miki208 <miloss208@gmail.com>
2016-12-13 16:03:36 +0100

    Update program.c (implement third query)

--- program.c
+++ program.c
@@ -19,7 +19,7 @@ const char* db = "online_kursevi";
 int main() {
     int quit = 0, i;
     char query[QUERYLEN];
-    char input[UINPUTLEN];
+    char input[UINPUTLEN], kor_ime[46], ime[46], prezime[46], email[46];
     MYSQL* conn = mysql_init(NULL);
     MYSQL_RES* result_set;
     MYSQL_FIELD* field;
@@ -50,7 +50,7 @@ int main() {
                 fgets(input, UINPUTLEN, stdin);
                 input[strlen(input) - 1] = '\0';
                 
-                sprintf(query, "SELECT ime, prezime, naziv, progres, datum_pocetka, datum_kraja FROM Ucesnik JOIN Korisnik ON Korisnik_kor_ime = kor_ime JOIN Prijavljuje ON Ucesnik_Korisnik_kor_ime = Korisnik_kor_ime JOIN Ciklus ON datum_pocetka = Ciklus_datum_pocetka AND Kurs_Sifra = Ciklus_Kurs_sifra JOIN Kurs ON sifra = Kurs_sifra WHERE Korisnik_kor_ime = '%s'", input);
+                sprintf(query, "SELECT ime, prezime, naziv, progres, datum_pocetka, datum_kraja FROM Ucesnik JOIN Korisnik ON Korisnik_kor_ime = kor_ime LEFT JOIN Prijavljuje ON Ucesnik_Korisnik_kor_ime = Korisnik_kor_ime LEFT JOIN Ciklus ON datum_pocetka = Ciklus_datum_pocetka AND Kurs_Sifra = Ciklus_Kurs_sifra LEFT JOIN Kurs ON sifra = Kurs_sifra WHERE Korisnik_kor_ime = '%s'", input);
                 
                 if(mysql_query(conn, query))
                     error_fatal("Upit 1: %s\n", mysql_error(conn));
@@ -88,8 +88,21 @@ int main() {
                 mysql_free_result(result_set);
                 break;
             case 3:
+                getchar();
+                printf("Unesite novo korisnicko ime, ime, prezime i email: ");
+                scanf("%s %s %s %s", kor_ime, ime, prezime, email);
+                
+                sprintf(query, "INSERT INTO Korisnik(kor_ime, ime, prezime, email) VALUES ('%s', '%s', '%s', '%s')", kor_ime, ime, prezime, email);
+                if(mysql_query(conn, query))
+                    error_fatal("Upit 3: %s\n", mysql_error(conn));
+                
+                sprintf(query, "INSERT INTO Ucesnik(Korisnik_kor_ime, poeni) VALUES ('%s', 0)", kor_ime);
+                if(mysql_query(conn, query))
+                    error_fatal("Upit 3: %s\n", mysql_error(conn));
+                getchar();
                 break;
             case 4:
+                getchar();
                 break;
             case 5:
                 break;
