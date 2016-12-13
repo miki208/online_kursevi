b9f1f37f06bda443abc792a7eac437392a70ba74
miki208 <miloss208@gmail.com>
2016-12-13 01:44:09 +0100

    Update program.c (implement first query)

--- program.c
+++ program.c
@@ -4,6 +4,10 @@
 #include <mysql.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <stdint.h>
+
+#define QUERYLEN 1024
+#define UINPUTLEN 100
 
 void error_fatal(char *format, ...);
 
@@ -13,8 +17,13 @@ const char* password = "";
 const char* db = "online_kursevi";
 
 int main() {
-    int quit = 0;
-    MYSQL *conn = mysql_init(NULL);
+    int quit = 0, i;
+    char query[QUERYLEN];
+    char input[UINPUTLEN];
+    MYSQL* conn = mysql_init(NULL);
+    MYSQL_RES* result_set;
+    MYSQL_FIELD* field;
+    MYSQL_ROW row;
     
     if(mysql_real_connect(conn, host, user, password, db, 0, NULL, 0) == NULL)
         error_fatal("Greska u konekciji: %s\n", mysql_error(conn));
@@ -22,6 +31,8 @@ int main() {
     while(!quit) {
         int opcija;
         
+        system("clear");
+        printf("-------------------------------------\n");
         printf("Odaberite jednu od opcija:\n");
         printf("\t[1] - Izlistati informacije o korisniku, kurseve koje slusa i njihove informacije\n");
         printf("\t[2] - Izlistati informacije o ucesnicima koji su dobili sertifikat, i informacije o odgovarajucim kursevima\n");
@@ -34,6 +45,27 @@ int main() {
         scanf("%d", &opcija);
         switch(opcija) {
             case 1:
+                getchar();
+                printf("Unesite korisnicko ime: ");
+                fgets(input, UINPUTLEN, stdin);
+                input[strlen(input) - 1] = '\0';
+                
+                sprintf(query, "SELECT ime, prezime, naziv, progres, datum_pocetka, datum_kraja FROM Ucesnik JOIN Korisnik ON Korisnik_kor_ime = kor_ime JOIN Prijavljuje ON Ucesnik_Korisnik_kor_ime = Korisnik_kor_ime JOIN Ciklus ON datum_pocetka = Ciklus_datum_pocetka AND Kurs_Sifra = Ciklus_Kurs_sifra JOIN Kurs ON sifra = Kurs_sifra WHERE Korisnik_kor_ime = '%s'", input);
+                
+                if(mysql_query(conn, query))
+                    error_fatal("Upit 1: %s\n", mysql_error(conn));
+                
+                result_set = mysql_use_result(conn);
+                int num_fields = mysql_num_fields(result_set);
+                field = mysql_fetch_fields(result_set);
+                
+                while((row = mysql_fetch_row(result_set)) != NULL) {
+                    printf("--------------------\n");
+                    for(i = 0; i < num_fields; i++)
+                        printf("%-20s| %s\n", field[i].name, row[i]);
+                }
+                
+                mysql_free_result(result_set);
                 break;
             case 2:
                 break;
@@ -50,6 +82,10 @@ int main() {
                 break;
             default:
                 printf("Ova opcija ne postoji\n");
+        }
+        if(!quit) {
+            printf("Da se vratite na glavni meni, pritisnite enter ");
+            getchar();
         }
     }
     
